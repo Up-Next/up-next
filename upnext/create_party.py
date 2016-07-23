8b5ff81cd70dc1d599b1fb50a3d82f5688a83412
VOjha <vidushi.ojha@gmail.com>
2016-07-23 14:31:02 -0700

    Migrations were being strange, so started fresh.

--- upnext/create_party.py
+++ upnext/create_party.py
@@ -1,5 +1,4 @@
 import spotipy
-from django.utils import timezone
 import spotipy.util as util
 from django.conf import settings
 import tokens
@@ -9,24 +8,24 @@ from .models import Party
 def create_party_in_db(request, form):
     new_party = form.save(commit=False)
     new_party.username = request.user.username
-    new_party.created_at = timezone.now()
     new_party.save()
     create_playlist(request, new_party.party_name)
     return new_party
 
+
 def create_playlist(request, party_name):
-  username = 'up--next'
-  user = request.user.social_auth.get(provider='spotify')
-  print user.extra_data['access_token'], '\n\n\n', user.extra_data['refresh_token']
-  token_info = tokens.token_read()
-  token = token_info['ACCESS_TOKEN']
-  refresh = token_info['REFRESH_TOKEN']
-  if token:
-      sp = spotipy.Spotify(auth=token)
-      sp.trace = False
-      playlist = sp.user_playlist_create(username, party_name)
-      party = Party.objects.get(party_name=party_name)
-      party.uri = playlist['uri']
-      party.save()
-  else:
-      print("Can't get token for", username)
+    username = 'up--next'
+    user = request.user.social_auth.get(provider='spotify')
+    print user.extra_data['access_token'], '\n\n\n', user.extra_data['refresh_token']
+    token_info = tokens.token_read()
+    token = token_info['ACCESS_TOKEN']
+    refresh = token_info['REFRESH_TOKEN']
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+        playlist = sp.user_playlist_create(username, party_name)
+        party = Party.objects.get(party_name=party_name)
+        party.uri = playlist['uri']
+        party.save()
+    else:
+        print("Can't get token for", username)
--- upnext/models.py
+++ upnext/models.py
@@ -8,22 +8,22 @@ class Party(models.Model):
     party_allowed = RegexValidator(r'^[A-Za-z0-9-_]+$', 'Permitted characters: alphanumeric, dash, underscore.')
     nickname_allowed = RegexValidator(r'^[A-Za-z]+$', 'Permitted characters: alphabet.')
 
-    party_name = models.CharField(max_length=140, validators=[party_allowed], default='', primary_key=True)
-    user_nickname = models.CharField(max_length=100, validators=[nickname_allowed], default='')
-    username = models.CharField(max_length=200, default='No Username')
+    party_name = models.CharField(max_length=100, validators=[party_allowed], default='', primary_key=True)
+    user_nickname = models.CharField(max_length=80, validators=[nickname_allowed], default='')
+    username = models.CharField(max_length=100, default='')
+    uri = models.CharField(max_length=140, default='')
     created_at = models.DateTimeField(default=timezone.now())
-    uri = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.party_name
 
 
 class Track(models.Model):
-    track_title = models.CharField(max_length=200, default='Unnamed Track')
-    artist = models.CharField(max_length=200, default='Unknown Artist')
-    score = models.IntegerField(default=0)
-    uri = models.CharField(max_length=200, default='')
-    party = models.ForeignKey(Party, default=0)
+    track_title = models.CharField(max_length=100, default='')
+    artist = models.CharField(max_length=100, default='')
+    score = models.IntegerField(default=1)
+    uri = models.CharField(max_length=140, default='')
+    party = models.ForeignKey(Party, default=None)
 
     def __str__(self):
         return self.track_title + ", by " + self.artist
