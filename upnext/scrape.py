b893d8e37d54dac630fa7903aa13614245d8a809
Vanessa Siri <vsiriwal@andrew.cmu.edu>
2016-07-27 12:20:39 -0700

    someone else take the wheel

--- upnext/scrape.py
+++ upnext/scrape.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from .models import Party, Track
 import spotipy
 import tokens
-
+from urllib2 import HTTPError
 
 def get_currently_playing(party, embed):
   token_info = tokens.token_read()
@@ -15,16 +15,31 @@ def get_currently_playing(party, embed):
   # playlist_url = get_playlist_url()
   party_id = party.uri.split(':')[-1]
 
-  profile = webdriver.FirefoxProfile()
-  profile.set_preference("webdriver.load.strategy", "unstable")
-  browser = webdriver.Firefox(firefox_profile=profile)
+  # profile = webdriver.FirefoxProfile()
+  # profile.set_preference("webdriver.load.strategy", "unstable")
+  print "start phantom"
+  browser = webdriver.PhantomJS()
+  browser.set_window_size(1280,1024)
+  browser.implicitly_wait(5)
+  browser.set_page_load_timeout(5)
   browser.get(embed)
+
+  print embed
   time.sleep(3)
+  # try:
+
+  #   # wait = WebDriverWait(driver, 10).until(
+  #   #   EC.presence_of_element_located((By.CSS_SELECTOR, "li.track-row"))
+  #   # )
+  # except:
+  #   print "oh no"
+  print browser.page_source
   bond_source = browser.page_source
   browser.quit()
   track_in_db = None
   soup = BeautifulSoup(bond_source, "html.parser")
   results = soup.findAll("li", {"class": "track-row playing"})
+  print results
   if results:
     now_playing = results[0]
     print now_playing
