c0acc41b3c881a27ff0cab6ca7250c6ca57f232d
Vanessa Siri <vsiriwal@andrew.cmu.edu>
2016-07-29 14:39:13 -0700

    prettier??

--- upnext/forms.py
+++ upnext/forms.py
@@ -3,7 +3,8 @@ from .models import Party
 
 
 class PartyForm(forms.ModelForm):
-
+    required_css_class = "required"
+    error_css_class = "error"
     class Meta:
         model = Party
         fields = ('party_name', 'url', 'user_nickname',)
