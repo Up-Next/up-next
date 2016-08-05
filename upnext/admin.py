bf1958b8fd11b96479b4a841a1e5d593c5dc766e
VOjha <vidushi.ojha@gmail.com>
2016-08-05 11:52:45 -0700

    Admin user access

--- upnext/admin.py
+++ upnext/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Track
+from .models import Track, Party, Voter
 
 admin.site.register(Track)
+admin.site.register(Party)
+admin.site.register(Voter)
